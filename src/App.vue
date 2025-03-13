c05eb4e890cd171c8253a949ad5afcbd593f84ee
Howpig <Howpig.coder@gmail.com>
2025-03-13 18:44:48 +0800

    awd

--- src/App.vue
+++ src/App.vue
@@ -95,7 +95,7 @@
     },
 
     logout() {
-      document.cookie = "access=; path=/; domain=.k12edu.uk; expires=Thu, 01 Jan 1970 00:00:00 GMT";
+      document.cookie = "access=; path=/; domain=api.k12edu.uk; expires=Thu, 01 Jan 1970 00:00:00 GMT";
 
       this.access_token = "";
       this.isLogin = false;
