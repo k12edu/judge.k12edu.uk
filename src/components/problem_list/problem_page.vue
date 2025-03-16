78e882a0be7da767c446b85abb70b7a6f565427f
Howpig2003 <106654207+Howpig2003@users.noreply.github.com>
2025-03-17 00:17:33 +0800

    Update problem_page.vue

--- src/components/problem_list/problem_page.vue
+++ src/components/problem_list/problem_page.vue
@@ -325,7 +325,7 @@ export default {
   border-image-repeat: repeat;
 }
 .l-select{
-  background-color: #f9f9f9;
+  border-radius: 8px;
   border-image-source: url('@/assets/tile_0000.png');
   border-width: 10px;
   border-style: solid;
